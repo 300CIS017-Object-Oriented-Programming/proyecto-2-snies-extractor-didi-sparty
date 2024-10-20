c017093aa5d509303cf6811a7e5754e0632f8865
Jorge Osorio <jorluos@hotmail.com>
2024-10-20 10:37:32 -0500

    MOD: CLASE PADRE

--- src/WriteManager.h
+++ src/WriteManager.h
@@ -13,14 +13,9 @@ using namespace std;
 class WriteManager{
     public:
         WriteManager();
-        vector<int> leerProgramasCsv(string &ruta);
-        vector<vector<string>> leerArchivoPrimera(string &rutaBase, string &ano, vector<int> &codigosSnies);
-        vector<vector<string>> leerArchivoSegunda(string &rutaBase, string &ano, vector<int> &codigosSnies);
-        vector<vector<string>> leerArchivo(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies);
         virtual void newFile(const string&) = 0;
         virtual bool loadFromFile(const string&) = 0;
         virtual bool saveToFile(const string&) = 0;
-
 };
 
 #endif
