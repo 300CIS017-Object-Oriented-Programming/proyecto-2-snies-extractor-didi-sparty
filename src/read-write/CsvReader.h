6c18894fd1f9407e0b31a94bad73c974b30bfec7
samuelbonillaf <samuelbonillaf@javerianacali.edu.co>
2024-10-20 14:08:35 -0500

    FIX: #includes CsvReader.h

--- src/read-write/CsvReader.h
+++ src/read-write/CsvReader.h
@@ -6,6 +6,7 @@ using namespace std;
 
 #include <string>
 #include <vector>
+#include <iostream>
 #include <map>
 #include <fstream>
 #include <sstream>
@@ -14,11 +15,18 @@ using namespace std;
 #include "ProgramaAcademico.h"
 #include "Consolidado.h"
 
+
 class CsvReader
 {
 public:
     CsvReader() = default;
+    /*
+     * @brief Lee el archivo programas.csv y busca los codigos SNIES que hay que filtrar
+     * @return Devuelve un vector<int> con los codigos SNIES
+     */
+    //FIXME: actualmente tiene parametros pero hay que quitarselos y reemplazarlo por #include settings
     vector<int> leerProgramasCsv(string &ruta);
+
     vector<vector<string>> leerArchivo(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies);
     vector<vector<string>> leerArchivoPrimera(string &rutaBase, string &ano, vector<int> &codigosSnies);
     vector<vector<string>> leerArchivoSegunda(string &rutaBase, string &ano, vector<int> &codigosSnies);
