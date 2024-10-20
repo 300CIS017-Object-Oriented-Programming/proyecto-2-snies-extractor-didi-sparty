09354ffdd02232d5615d4f14a22094d42eed61a9
dacru0605 <dacru06050@gmail.com>
2024-10-20 10:42:50 -0500

    MOD: Se corrigen metodos de validacion

--- src/View.cpp
+++ src/View.cpp
@@ -20,6 +20,43 @@ View::~View()
     controlador.~SNIESController();
 }
 
+char View::obtenerEntradaUsuario(const string &mensaje, const string &opcionesValidas)
+{
+    char entrada;
+    while (int i = 0)
+    {
+        cout << mensaje << endl;
+        cin >> entrada;
+        entrada = tolower(entrada);
+        if (opcionesValidas.find(entrada) != string::npos) i=1;
+        else cout << "Entrada no válida. Intente de nuevo." << endl;
+    }
+    return entrada;
+}
+
+string View::obtenerAnoValido(const string &mensaje)
+{
+    string ano;
+    while (int i = 0)
+    {
+        cout << mensaje << endl;
+        cin >> ano;
+        if (isConvetibleToInt(ano)) i=1;
+        else cout << "El valor ingresado fue invalido! Por favor ingrese un valor valido." << endl;
+    }
+    return ano;
+}
+
+void View::intercambiarAnos(string &ano1, string &ano2)
+{
+    if (stoi(ano2) < stoi(ano1))
+    {
+        string temp = ano1;
+        ano1 = ano2;
+        ano2 = temp;
+    }
+}
+
 bool View::mostrarPantallaBienvenido()
 {
     bool parametrizacionBool = false;
@@ -29,14 +66,9 @@ bool View::mostrarPantallaBienvenido()
     cout << "Recuerde que para el correcto funcionamiento del programa tuvo que haber parametrizado" << endl;
     cout << "antes la carpeta SNIES_EXTRACTOR en el disco duro C:, con sus respectivas carpetas inputs y outputs" << endl;
     cout << "y todos los archivo CSV del SNIES." << endl;
-    cout << "Si ya hizo esto, escriba 'Y', de lo contrario 'N', y Enter: " << endl;
-    char userAnswer = 'Y'; // FIXME cuando se arregle el debugger
-    // cin >> userAnswer;
-    // cout << endl;
-    // FIXME verificar que el usuario ingree un valor igual al esperado incluir todo dentro de un while para
-    // para asegurar que el usuario ingrese un valor valido
-    // pasarlo a un método que se pueda usar en otros lugares
-    userAnswer = static_cast<char>(tolower(userAnswer));
+    
+    char userAnswer = obtenerEntradaUsuario("Si ya hizo esto, escriba 'Y', de lo contrario 'N', y Enter: ", "yn");
+
     if (userAnswer == 'y')
     {
         parametrizacionBool = true;
@@ -44,57 +76,10 @@ bool View::mostrarPantallaBienvenido()
         string userText;
         cout << "A continuacion se procesaran los datos de los programas academicos seleccionados en /programas.csv..." << endl;
 
-        string anio1("abc");
-        string ano2("abc");
-        string anoAux;
-        int i = 0;
-        bool anosValido = false;
-        // FIXME pasar la lógica del bucle a un método reutlizable
-        // Usar en el while una bandera y simplificar el código
-        // Bucle para leer un valor valido del año1
-        while (!(isConvetibleToInt(anio1)))
-        {
-            if (i == 1)
-            {
-                cout << "El valor ingresado fue invalido!" << endl;
-                cout << "Por favor ingrese un valor valido la proxima" << endl;
-                cout << "Presione 'OK' y Enter para continuar: " << endl;
-                cin >> userText;
-                cout << endl;
-            }
-            cout << "Escriba el primer ano de busqueda: " << endl;
-            cin >> anio1;
-            cout << endl;
-            i = 1;
-        }
-
-        i = 0;
-        // Bucle para leer un valor valido del año2
-        while (!(isConvetibleToInt(ano2)))
-        {
-            if (i == 1)
-            {
-                cout << "El valor ingresado fue invalido!" << endl;
-                cout << "Por favor ingrese un valor valido la proxima" << endl;
-                cout << "Presione 'OK' y Enter para continuar: " << endl;
-                cin >> userText;
-                cout << endl;
-            }
-            cout << "Escriba el segundo ano de busqueda: " << endl;
-            cin >> ano2;
-            cout << endl;
-            i = 1;
-        }
-
-        // Organizo los años
-        // FIXME: Crear un método para hacer que el segundo año sea siempre
-        // mayor que el primer año
-        if (stoi(ano2) < stoi(anio1))
-        {
-            anoAux = anio1;
-            anio1 = ano2;
-            ano2 = anoAux;
-        }
+        string anio1 = obtenerAnoValido("Escriba el primer ano de busqueda: ");
+        string ano2 = obtenerAnoValido("Escriba el segundo ano de busqueda: ");
+        
+        intercambiarAnos(anio1, ano2);
 
         cout << "Procesando datos ..." << endl;
         controlador.procesarDatosCsv(anio1, ano2);
@@ -114,13 +99,8 @@ void View::mostrarDatosExtra()
 {
     char opcionYN;
     cout << "A continuacion vamos a mostrar datos relevantes de los programas academicos seleccionados" << "\n"
-         << endl;
-    cout << "Desea Convertir los datos a un archivo CSV?(Y/N): " << endl;
-    cin >> opcionYN;
-    opcionYN = tolower(opcionYN);
-    cout << "\n";
-    // FIXME verificar que el usuario ingrese un valor igual al esperado, return true si es Y, false si es N, y no sale si no retorna un valor válido
-    // Simplificar el código de acuerdo a ese ajuste
+        << endl;
+    char opcionYN = obtenerEntradaUsuario("Desea Convertir los datos a un archivo CSV?(Y/N): ", "yn");
     if (opcionYN == 'y')
     {
         controlador.calcularDatosExtra(true);
@@ -173,7 +153,7 @@ void View::buscarPorPalabraClaveYFormacion()
             while ((idFormacionAcademica > 10) || (idFormacionAcademica == 9) || (idFormacionAcademica < 1))
             {
                 cout << "Seleccione una opcion entre 1-10 excluyendo el 9\n"
-                     << endl;
+                    << endl;
                 cin >> idFormacionAcademica;
             }
 
