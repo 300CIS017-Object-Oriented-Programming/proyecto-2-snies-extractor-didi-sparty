aebb16eb3dea1bc54d6591083bd9cc97a7a28a23
Alejandro <alejandrosantandertoto@gmail.com>
2024-10-20 02:03:37 -0500

    MOD: algunas otras modificaciones

--- src/CsvReader.h
+++ src/CsvReader.h
@@ -13,7 +13,6 @@ public:
     vector<vector<string>> leerArchivo(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies) override;
     vector<vector<string>> leerArchivoPrimera(string &rutaBase, string &ano, vector<int> &codigosSnies);
     vector<vector<string>> leerArchivoSegunda(string &rutaBase, string &ano, vector<int> &codigosSnies);
-    
 };
 
 #endif
