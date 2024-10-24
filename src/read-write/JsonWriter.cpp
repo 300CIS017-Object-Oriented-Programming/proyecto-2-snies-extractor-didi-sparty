94e4bd76982f140c6c6173898e048a73a1eb436d
Samuel Alberto Mateo Bonilla Franco <samuelbonillaf@javerianacali.edu.co>
2024-10-24 02:05:43 -0500

    NEW: YAISON YAISON YAISON

--- src/read-write/JsonWriter.cpp
+++ src/read-write/JsonWriter.cpp
@@ -3,3 +3,22 @@
 
 
 JsonWriter::JsonWriter() = default;
+bool JsonWriter::crearArchivo(string &ruta, map<int, ProgramaAcademico *> &mapadeProgramasAcademicos, vector<string> etiquetasColumnas) {
+    ofstream file(ruta);
+    if (file.is_open()) {
+        file << "{}";
+        file << jsonData.dump(4);
+        file.close();
+    }
+    return true;
+}
+bool JsonWriter::crearArchivoBuscados(string &ruta, list<ProgramaAcademico *> &programasBuscados, vector<string> etiquetasColumnas) {
+    return true;
+}
+
+
+bool JsonWriter::crearArchivoExtra(string &ruta, vector<vector<string>> datosAImprimir){return true;}
+
+
+
+
